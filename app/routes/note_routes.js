2a895769a547c5cf99e8da9aa529653006323a31
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-04-15 20:38:23 +0100

    a very simple api that perform CRUD using mongodb and node

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,44 @@
-module.exports = function(app, db) {
+const ObjectID = require('mongodb').ObjectID
+
+module.exports = function(app, client) {
+    const db = client.db('api-test')
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = {'_id': new ObjectID(id)}
+        db.collection('notes').findOne(details, (err, items) => {
+            if(err) {
+                res.send({ 'error': 'An error has occurred'})
+            } else {
+                res.send(items)
+            }
+        })
+    })
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = {'_id': new ObjectID(id)}
+        db.collection('notes').remove(details, (err, items) => {
+            if(err) {
+                res.send({ 'error': 'An error has occurred'})
+            } else {
+                res.send(`Note ${id} deleted!!`)
+            }
+        })
+    })
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = {'_id': new ObjectID(id)}
+        const note = {text: req.body.body, title: req.body.title}
+        db.collection('notes').update(details, note, (err, items) => {
+            if(err) {
+                res.send({ 'error': 'An error has occurred'})
+            } else {
+                res.send(items)
+            }
+        })
+    })
+
     app.post('/notes', (req, res) => {
         const note = {text: req.body.body, title: req.body.title}
         db.collection('notes').insert(note, (err, results) => {
